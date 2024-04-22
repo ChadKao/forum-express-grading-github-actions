ea40f1cd5b09040797681d8fe9e0f26747096068
ChadKao <coolchad2000@gmail.com>
2024-04-22 12:08:25 +0800

    feat: add categories selector on admin create and edit page

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -3,5 +3,8 @@ module.exports = {
   currentYear: () => dayjs().year(),
   ifCond: function (a, b, options) {
     return a === b ? options.fn(this) : options.inverse(this)
+  },
+  eq: (arg1, arg2) => {
+    return arg1 === arg2
   }
 }
