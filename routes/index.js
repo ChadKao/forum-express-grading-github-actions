c7d9941943a788b145613291f20b79e83fb87e5e
ChadKao <coolchad2000@gmail.com>
2024-06-13 15:06:42 +0800

    feat: add like/unlike function

--- routes/index.js
+++ routes/index.js
@@ -30,6 +30,8 @@ router.get('/users/:id/edit', authenticated, userController.editUser)
 router.put('/users/:id', upload.single('image'), authenticated, userController.putUser)
 router.post('/favorite/:restaurantId', authenticated, userController.addFavorite)
 router.delete('/favorite/:restaurantId', authenticated, userController.removeFavorite)
+router.post('/like/:restaurantId', authenticated, userController.addLike)
+router.delete('/like/:restaurantId', authenticated, userController.removeLike)
 
 router.use('/', (req, res) => {
   req.flash('error_messages', res.locals.error_messages[0])
